16ea1b292022d61bf6e422a6f9c43e75516ee489
Cedric Cellier <rixed@happyleptic.org>
2017-05-16 15:15:18 +0200

    Remove useless (and misleading) PPP_CSV.sep

--- PPP_CSV.ml
+++ PPP_CSV.ml
@@ -8,23 +8,21 @@ struct
 
   let unit : unit P.t = cst ""
 
-  let sep = "|"
-
-  let list (ppp : 'a P.t) : 'a list P.t =
-    P.seq "list" "" "" sep List.fold_left List.rev ppp
-
-  let array (ppp : 'a P.t) : 'a array P.t =
-    P.seq "array" "" "" sep Array.fold_left (fun l -> Array.of_list (List.rev l)) ppp
-
   let tuple_open = ""
   let tuple_close = "\n"
   let tuple_sep = "|"
 
+  let list (ppp : 'a P.t) : 'a list P.t =
+    P.seq "list" "" "" tuple_sep List.fold_left List.rev ppp
+
+  let array (ppp : 'a P.t) : 'a array P.t =
+    P.seq "array" "" "" tuple_sep Array.fold_left (fun l -> Array.of_list (List.rev l)) ppp
+
   let pair (p1 : 'a P.t) (p2 : 'b P.t) : ('a * 'b) P.t =
-    p1 +- cst sep ++ p2
+    p1 +- cst tuple_sep ++ p2
 
   let triple (p1 : 'a P.t) (p2 : 'b P.t) (p3 : 'c P.t) : ('a * 'b * 'c) P.t =
-    p1 +- cst sep ++ p2 +- cst sep ++ p3 >>:
+    p1 +- cst tuple_sep ++ p2 +- cst tuple_sep ++ p3 >>:
       ((fun (v1,v2,v3) -> (v1,v2),v3),
        (fun ((v1,v2),v3) -> v1,v2,v3))
 end
