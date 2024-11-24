680665650a14ba5267db0432bb58c9938ad1fb6e
Cedric Cellier <rixed@happyleptic.org>
2024-11-24 11:58:48 +0100

    Bump version to 2.10.0

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "ocaml${ocaml.version}-pretty-printers-parsers";
-  version = "2.9.0";
+  version = "2.10.0";
 
   src = fetchFromGitHub {
     owner = "rixed";
